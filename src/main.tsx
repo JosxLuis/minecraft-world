8e45c9fea91a8c8a6490b7b06aa1fb657fa126bb
Rey Vega <reynaldovega2010@hotmail.com>
2022-01-31 19:42:47 -0600

    Fixes menu bug

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import Stats from "three/examples/jsm/libs/stats.module";
-
+import "./index.css"
 
 //-----------------------------------------------------------
 //-- Create scene's base
